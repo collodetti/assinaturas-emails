6fff346d1177171c21d20e5a805f665069cfe18b
Flávio <>
2023-04-18 11:37:01 -0300

    fix: page designer error

--- src/js/email.js
+++ src/js/email.js
@@ -4,34 +4,38 @@ let cel = document.getElementById("cel")
 let pCel = document.getElementById("p-cel")
 let tel = document.getElementById("tel");
 let skype = document.getElementById("skype");
-let linkSkype = document.getElementById("link-skype");
 let email = document.getElementById("email");
+let linkCel = document.getElementById("link-cel");
+let linkTel = document.getElementById("link-tel");
+let linkSkype = document.getElementById("link-skype");
 let linkEmail = document.getElementById("link-email");
 
 
 let pessoaString = localStorage.getItem('pessoa');
 // transformar em objeto novamente
 let pessoaObj = JSON.parse(pessoaString);
-const departamento = pessoaObj.departamento.toUpperCase();
-const obs = pessoaObj.obs;
+const departamento = pessoaObj.departamento.toUpperCase().trim();
+const obs = pessoaObj.obs.trim();
 
-nome.textContent = pessoaObj.nome.toUpperCase();
-cargo.textContent = pessoaObj.cargo;
-tel.textContent = "+55 " + pessoaObj.tel;
+nome.textContent = pessoaObj.nome.toUpperCase().trim();
+cargo.textContent = pessoaObj.cargo.trim();
+tel.textContent = "+55 " + pessoaObj.tel.trim();
+linkTel.href = "tel:+55" + pessoaObj.tel.replace("-", "").replaceAll(" ", "");
 
 if (departamento === 'CONTABILIDADE') {
   pCel.textContent = obs;
 } else if (pessoaObj.cel === "") {
   pCel.style.display = 'none';
 } else {
-  cel.textContent = "+55 " + pessoaObj.cel;
+  cel.textContent = "+55 " + pessoaObj.cel.trim();
+  linkCel.href = "tel:+55" + pessoaObj.cel.replace("-", "").replaceAll(" ", "");
 }
 
 if (pessoaObj.skype === "") {
   linkSkype.style.display = 'none';
 } else {
   skype.textContent = pessoaObj.skype;
-  linkSkype.href = `skype:${pessoaObj.skype}?chat`;
+  linkSkype.href = `skype:live:${pessoaObj.skype}?chat`;
 }
 
 if (pessoaObj.email === "") {
